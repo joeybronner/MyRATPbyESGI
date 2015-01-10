c37f426f11eca3485fc266f7b1cad639eea5f500
Joey Bronner <joeybronner@gmail.com>
2015-01-10 23:31:44 +0100

    SplashScreen implementation on launch

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MyRATPbyESGI
 Android project (5th year @ESGI) using public Open Data by RATP
+
+App Storyboard (2014-01-11)
+-------
+
+http://www.mockflow.com/
---
+++ src/fr/esgi/ratp/SplashScreen.java
@@ -0,0 +1,28 @@
+package fr.esgi.ratp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+
+	// Splash screen timer
+	private static int SPLASH_TIME_OUT = 1500;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash_screen);
+
+		new Handler().postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				Intent i = new Intent(SplashScreen.this, MainActivity.class);
+				startActivity(i);
+				finish();
+			}
+		}, SPLASH_TIME_OUT);
+	}
+}
